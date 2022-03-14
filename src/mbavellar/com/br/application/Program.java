1d493086cb73f29bd838049bc1b4fd5bcdbc93cd
Mauricio Avellar <86550730+mbavellar@users.noreply.github.com>
2022-03-14 12:57:18 -0300

    Department entity class

--- src/mbavellar/com/br/application/Program.java
+++ src/mbavellar/com/br/application/Program.java
@@ -1,8 +1,12 @@
 package mbavellar.com.br.application;
 
+import mbavellar.com.br.model.entities.Department;
+
 public class Program {
   
   public static void main (String[] args) {
   
+    Department department = new Department(1, "Books");
+    System.out.println(department);
   }
 }
